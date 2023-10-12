385acb1e561459b1628861a796f9d3fb1b564ad7
leemingoo <dwmapi@naver.com>
2023-10-12 11:58:05 +0900

    테스트

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("test");
+  res.write("testtest");
   res.end();
 });
 
