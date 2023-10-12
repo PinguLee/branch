9f4b63e44249b21b988d53bfe37c9ae261189a39
leemingoo <dwmapi@naver.com>
2023-10-12 11:43:44 +0900

    write 내용 변경

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("pingu");
+  res.write("pingutesttest");
   res.end();
 });
 
